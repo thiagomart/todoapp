2b2bc1f004f8895f612cf65e41c0f226646b4b2b
thiagomart <thiagomsl@gmail.com>
2018-06-23 20:52:56 -0300

    Criação da Classe Context

---
+++ Data/TodoContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Models;
+
+
+namespace TodoApp.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options){
+
+        }
+
+        public DbSet<TodoItem> TodoItems {get; set;}
+    }
+}
