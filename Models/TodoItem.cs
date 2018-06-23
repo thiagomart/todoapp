b92fbe2f60d69dfea544fa608455ff45d850a78d
thiagomart <thiagomsl@gmail.com>
2018-06-23 20:22:45 -0300

    Criação de Models e adiação de arquivos de .gitignore

---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace TodoApp.Models
+{
+    public class TodoItem
+    {
+        public long id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
